d761b938b968db6539288a354082192130e3c741
Seungyun Oh <syoh0708@kaist.ac.kr>
2023-08-02 12:11:09 +0900

    Refactor Dataset and implement saving and loading model

--- datasets/credit_card_fraud_detection/commons/__init__.py
+++ datasets/credit_card_fraud_detection/commons/__init__.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import confusion_matrix, fbeta_score, roc_auc_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from datetime import datetime
-from typing import Any, Dict
+from typing import Any, Dict, Callable
 from pandas import DataFrame
 import mlflow
 
@@ -13,9 +13,9 @@ class Dataset:
         self.X_test = None
         self.X_train = None
         self.df = df
-        self.metrics = metrics
         self.scaler = scaler
         self.label_col = label_col
+        self.__ready = False
 
         self.X = df.drop(df.columns[label_col], axis=1)
         self.y = df.iloc[:, label_col]
@@ -24,7 +24,7 @@ class Dataset:
         if self.scaler is not None:
             self.X = self.scaler.fit_transform(self.X)
 
-    def split(self, test_size=0.3, random_state=0):
+    def split(self, test_size: float = 0.3, random_state: int = 0):
         X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=test_size,
                                                             random_state=random_state, stratify=self.y)
 
@@ -32,29 +32,65 @@ class Dataset:
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
+        self.__ready = True
 
-    def log_metric(self, pred, pred_proba):
-        confusion = confusion_matrix(self.y_test, pred)
-        print('confusion_matrix')
-        print(confusion)
+    def scale_and_split(self, test_size: float = 0.3, random_state: int = 0):
+        self.scale()
+        self.split(test_size=test_size, random_state=random_state)
 
-        for name, func in self.metrics.items():
-            val = func(self.y_test, pred, pred_proba)
-            mlflow.log_metric(name, val)
-            print(f'{name}: {val}')
+    def is_ready(self):
+        return self.__ready
 
-    def eval(self, model, tag='default', experiment_name='default'):
+
+class Model:
+    def __init__(self, model: Any, dataset: Dataset):
+        self.model: Any = model
+        self.dataset: Dataset = dataset
+        self.is_trained = False
+        self.pred = None
+        self.pred_proba = None
+
+    def log_metric(self, metrics: Dict[str, Callable[[Any, Any, Any], float]], tag: str = 'default',
+                   experiment_name: str = 'default'):
+        if self.is_trained is False:
+            raise ValueError('Model is not trained. Call eval() first.')
+
         now = datetime.now()
-        algorithm_name = model.__class__.__name__
+        algorithm_name = self.model.__class__.__name__
         run_name = f'{algorithm_name}_{tag}_{now.strftime("%H:%M:%S")}'
         experiment = mlflow.get_experiment_by_name(experiment_name)
         experiment_id = mlflow.create_experiment(experiment_name) if experiment is None else experiment.experiment_id
 
+        confusion = confusion_matrix(self.dataset.y_test, self.pred)
+        print('confusion_matrix')
+        print(confusion)
+
         with mlflow.start_run(run_name=run_name, experiment_id=experiment_id):
-            model.fit(self.X_train, self.y_train)
-            pred = model.predict(self.X_test)
-            pred_proba = model.predict_proba(self.X_test)[:, 1]
-            self.log_metric(pred, pred_proba)
+            for name, func in metrics.items():
+                val = func(self.dataset.y_test, self.pred, self.pred_proba)
+                mlflow.log_metric(name, val)
+                print(f'{name}: {val}')
+
+    def save_model(self):
+        model_type = type(self.model)
+        if model_type.__module__.startswith('sklearn.'):
+            mlflow.sklearn.save_model(self.model, 'model')
+        elif model_type.__module__.startswith('lightgbm.'):
+            return mlflow.lightgbm.save_model(self.model, 'model')
+        else:
+            raise ValueError(f'Unsupported model type: {model_type}')
+
+    def eval(self, save_model: bool = False):
+        if self.dataset.is_ready() is False:
+            raise ValueError('Dataset is not ready. Call scale_and_split() first.')
+
+        self.model.fit(self.dataset.X_train, self.dataset.y_train)
+        self.pred = self.model.predict(self.dataset.X_test)
+        self.pred_proba = self.model.predict_proba(self.dataset.X_test)[:, 1]
+        self.is_trained = True
+
+        if save_model:
+            self.save_model()
 
 
 metrics = {
